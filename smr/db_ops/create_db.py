00ab62dc14694c7a6b58bd60faaa17a6d08a26e1
hughharford <hugh.harford@poscoconsulting.com>
2022-06-27 16:42:47 +0100

    first local db created and data ingress and egress

--- smr/db_ops/create_db.py
+++ smr/db_ops/create_db.py
@@ -1,28 +1,55 @@
 import sqlite3
-from sql_commands import DB_TABLE_CREATE
+import pandas as pd
 
-DB_NAME = 'db/test_database'
+from sql_commands import DB_TABLE_CREATE, DB_TABLE_CREATE_SHORT
+from sql_commands import INSERT_INTO_SEARCHES, CONFIRM_DATA
+
+
+DB_NAME = 'db/test_database.sqlite'
 # db/test_database
 
 class SMR_Database():
 
-    def __init__(self):
-        pass
+    def __init__(self, connection):
+        self.conn = connection
 
     def create_database(self):
-        conn = sqlite3.connect(DB_NAME, timeout=3)
-        print('Connected to database...  ' + DB_NAME)
-        c = conn.cursor()
-
-        c.execute('''
-          CREATE TABLE IF NOT EXISTS products
-          ([product_id] INTEGER PRIMARY KEY, [product_name] TEXT)
-          ''')
+        self.c = self.conn.cursor()
+        self.c.execute(DB_TABLE_CREATE)
         print('Created table within database...  ' + DB_NAME)
+        self.conn.commit()
+        print('Commit made >>')
 
-        conn.commit()
-        conn.close()
+    def insert_data(self):
+        self.c.execute(INSERT_INTO_SEARCHES)
+        self.conn.commit()
+
+
+    def confirm_contents(self):
+        self.c.execute(CONFIRM_DATA)
+        df = pd.DataFrame(
+            self.c.fetchall(),
+            columns=['search_input','postcode', 'count_search'])
+        print (df)
 
 if __name__ == "__main__":
-    db = SMR_Database()
-    db.create_database()
+
+    conn = sqlite3.connect(DB_NAME, timeout=0.00001)
+    print('Connected to database...  ' + DB_NAME)
+
+    try:
+        db = SMR_Database(conn)
+        db.create_database()
+        db.insert_data()
+        db.confirm_contents()
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if conn != None:
+            pass
+    try:
+        conn.close() # <-- This is important
+    except Exception:
+        print(Exception)
--- smr/db_ops/sql_commands.py
+++ smr/db_ops/sql_commands.py
@@ -1,10 +1,41 @@
 # not needed in sqlite
 #                  DB_CREATE = 'CREATE DATABASE test_database'
 
-DB_TABLE_CREATE = '''CREATE TABLE searches (   /* Create table with multiple fields as unique */
-   ID INT NOT NULL
-   search_input VARCHAR(255) NOT NULL
-   postcode BOOLEAN NOT NULL
-   count_search INT(255)
+DB_TABLE_CREATE = '''CREATE TABLE IF NOT EXISTS searches
+(
+   ID INT PRIMARY KEY NOT NULL,
+   [search_input] TEXT NOT NULL,
+   [postcode] BOOLEAN NOT NULL,
+   [count_search] INTEGER,
    UNIQUE (ID, postcode)
 );'''
+
+DB_TABLE_CREATE_SHORT = '''CREATE TABLE IF NOT EXISTS searches
+(
+   ID INT PRIMARY KEY,
+   [search_input] TEXT NOT NULL,
+   [postcode] TEXT NOT NULL,
+   [count_search] INTEGER
+);'''
+
+
+INSERT_INTO_SEARCHES = '''
+        INSERT INTO searches (
+            search_input,
+            postcode,
+            count_search)
+
+        VALUES
+        ('SE23 3YL','SE23 3YL', 45),
+        ('G3 xxx','G3 xxx', 1001),
+        ('latt long','SE23 3YL', 23)
+        ;
+    '''
+
+CONFIRM_DATA = '''
+            SELECT
+            search_input,
+            postcode,
+            count_search
+            FROM searches
+          '''
